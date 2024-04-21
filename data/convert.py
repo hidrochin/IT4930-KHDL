19e556fb3e1c70efb7a7c6c4bf90a946ab81bfce
mthcxg <110339956+mthcxg@users.noreply.github.com>
2024-04-21 21:09:46 +0700

    update data negative & positive

--- data/convert.py
+++ data/convert.py
@@ -8,16 +8,38 @@ data_files = [
 ]
 
 dfs = []  
+df1 = []
 
 for file in data_files:
     df = pd.read_csv(file)
-    filtered_df = df[(df['rating'] <= 3) & df['content'].notnull()]
-    dfs.append(filtered_df)
+    df_negative = df[(df['rating'] <= 3) & df['content'].notnull()]
+    dfs.append(df_negative)
 
 merged_df = pd.concat(dfs)
 
 merged_df = merged_df.drop('customer_id', axis=1)
 
+merged_df = merged_df.drop('id', axis=1)
+
+merged_df = merged_df.drop('title', axis=1)
+
 non_duplicates_df = merged_df.drop_duplicates(subset='content')
 
-non_duplicates_df.to_csv('data/data.csv', index=False)
+non_duplicates_df.to_json('data/data-negative.json', orient='records', force_ascii=False)
+
+for file in data_files:
+    df = pd.read_csv(file)
+    df_positive = df[(df['rating'] > 3) & df['content'].notnull()]
+    df1.append(df_positive)
+
+merged_df = pd.concat(df1)
+
+merged_df = merged_df.drop('customer_id', axis=1)
+
+merged_df = merged_df.drop('id', axis=1)
+
+merged_df = merged_df.drop('title', axis=1)
+
+non_duplicates_df = merged_df.drop_duplicates(subset='content')
+
+non_duplicates_df.to_json('data/data-positive.json', orient='records', force_ascii=False)
